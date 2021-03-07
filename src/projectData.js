661253886fd62e80e65bee39cfc67d76c1851563
orangemn6 <jbensa081@gmail.com>
2021-03-06 22:02:04 -0500

    removed node modules

---
+++ src/projectData.js
@@ -0,0 +1,20 @@
+const projects = [
+  {
+    title: 'OpenSuspect',
+    image: './images/opensuspect.png',
+    description: 'Open Source social deduction game based on great works such as mafia.',
+    tools: ['Godot', 'Team management',],
+    github: 'https://github.com/opensuspect/opensuspect',
+    link: 'https://opensuspect.com',
+  },
+  {
+    title: 'EndoBox',
+    image: './images/endobox.png',
+    description: 'Bring your child’s online experience back into the real world.',
+    tools: ['Javascript', 'Java', 'HTML'],
+    github: 'https://github.com/',
+    link: 'https://endobox.net/',
+  },
+];
+
+export default projects;
